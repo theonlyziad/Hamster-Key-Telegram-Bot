69cb1203037c7c6a5a32614fa8689252219f2153
theonlyziad <132782844+theonlyziad@users.noreply.github.com>
2024-09-08 00:47:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,60 +2,32 @@ import subprocess
 import os
 import logging
 import asyncio
-from telegram import Update, ChatMember
+from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from datetime import datetime
 import server
+from config import TOKEN, AUTHORIZED_USERS, EXCLUSIVE, USE_PROXIES
 
-# Telegram Token
-TOKEN_INSECURE = "7537444054:AAE5PY_MRDEBGWdrm78xS133E8PpC_ygjJo"
-
-if os.name == 'posix':
-    TOKEN = subprocess.run(["printenv", "HAMSTER_BOT_TOKEN"], text=True, capture_output=True).stdout.strip()
-elif os.name == 'nt':
-    TOKEN = subprocess.run(["echo", "%HAMSTER_BOT_TOKEN%"], text=True, capture_output=True, shell=True).stdout.strip()
-    TOKEN = "" if TOKEN == "%HAMSTER_BOT_TOKEN%" else TOKEN
-
-# Telegram channel username
-CHANNEL_USERNAME = '@zeedtek'
-
-# List of authorized users if the bot is in exclusive mode
-AUTHORIZED_USERS = []
-EXCLUSIVE = False
-
-# Setup logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.WARN
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
-    
-    # Check if the user is a member of the channel
-    try:
-        member = await context.bot.get_chat_member(CHANNEL_USERNAME, user_id)
-        if member.status in ['member', 'administrator', 'creator']:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome! You are already a member of the channel.")
-        else:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"You need to join the channel {CHANNEL_USERNAME} to use this bot.")
-            return
-    except Exception as e:
-        logging.error(f"Error checking membership: {e}")
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"You need to join the channel {CHANNEL_USERNAME} to use this bot.")
-        return
-
+    # Should make this a Database probably
+    # with open(f'{os.path.dirname(__file__)}/user_ids','a') as file:
+    #     file.write(f"{datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} {update.effective_chat.first_name}, {update.effective_chat.username}, {update.effective_chat.id}\n")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="The available commands are:\n*/cube*\n*/train*\n*/fluf*\n*/merge*\n*/twerk*\n*/poly*\n*/trim*\n*/cafe*\n*/zoo*\n*/gang*\n*/all*\nThese will generate 4 keys for their respective games\.",
+        text="The Commands are:\n*/cube*\n*/train*\n*/merge*\n*/twerk*\n*/poly*\n*/trim*\n*/cafe*\n*/zoo*\n*/tile*\n*/fluff*\n*/stone*\n*/all*\nThese will generate 4 keys for their respective games\\. /fluff will generate 8 keys\\.",
         parse_mode='MARKDOWNV2'
-    )
+        )
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="You can also set how many keys are generated\. For example, */cube 8* will generate *EIGHT* keys for the cube game\.",
+        text="You can also set how many keys are generated\\. For example, */cube 8* will generate *EIGHT* keys for the cube game\\.",
         parse_mode='MARKDOWNV2'
-    )
+        )
 
 async def game_handler(
     update: Update, 
@@ -67,7 +39,7 @@ async def game_handler(
     if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
         await context.bot.send_message(
             chat_id=update.effective_chat.id, 
-            text="Clone this bot from [github](https://github.com/Emperor-One/Hamster-Key-Telegram-Bot) and follow the instructions to create your own bot\.",
+            text="Clone this bot from the [github](https://github.com/Emperor-One/Hamster-Key-Telegram-Bot) repo and follow the instructions to create your own bot\\.",
             parse_mode='MARKDOWNV2'
         )
         with open(f'{os.path.dirname(__file__)}/unauthorized','a') as file:
@@ -76,22 +48,28 @@ async def game_handler(
             file.write(f"{datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} {unauthorized_message}\n")
         return
 
+    # delay for the /all command
     await asyncio.sleep(delay)
     server.logger.info(f"Delay for {delay} seconds")
+
 
     server.logger.info(f"Generating for client: {update.effective_chat.first_name} - {update.effective_chat.username}: {update.effective_chat.id}")
     if not all:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Generating\.\.\.", parse_mode='MARKDOWNV2')
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"This will only take a moment\.\.\.", parse_mode='MARKDOWNV2')
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Join to @zeedtek channel for updates\.", parse_mode='MARKDOWNV2')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Generating\\.\\.\\.", parse_mode='MARKDOWNV2')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"This will only take a moment\\.\\.\\.", parse_mode='MARKDOWNV2')
 
-    no_of_keys = int(context.args[0]) if context.args else 4
-    keys = await server.run(chosen_game=chosen_game, no_of_keys=no_of_keys)
-    generated_keys = [f"`{key}`" for key in keys]
-    formatted_keys = '\n'.join(generated_keys)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{formatted_keys}", parse_mode='MARKDOWNV2')
-    server.logger.info("Message sent to the client.")
+    if server.GAMES[chosen_game]['name'] == "Fluff Crusade":
+        no_of_keys = 8
+    else:
+        no_of_keys = int(context.args[0]) if context.args else 4
+    
+    key_count = 0
+    async for key in server.run(chosen_game=chosen_game, no_of_keys=no_of_keys, use_proxies=USE_PROXIES):
+        formatted_key = f"`{key}`"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{key_count + 1}\\. {formatted_key}", parse_mode='MARKDOWNV2')
+        server.logger.info(f"Message sent to client: {update.effective_chat.first_name} - {update.effective_chat.username}: {update.effective_chat.id}")
+        key_count += 1
 
 async def cube(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
     await game_handler(update, context, chosen_game=1, all=all)
@@ -114,8 +92,14 @@ async def trim(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
 async def zoo(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
     await game_handler(update, context, chosen_game=7, all=all)
 
-async def fluf(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
+async def tile(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
     await game_handler(update, context, chosen_game=8, all=all)
+
+async def fluff(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
+    await game_handler(update, context, chosen_game=9, all=all)
+
+async def stone(update: Update, context: ContextTypes.DEFAULT_TYPE, all = False):
+    await game_handler(update, context, chosen_game=10, all=all)
 
 async def all(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
@@ -124,14 +108,16 @@ async def all(update: Update, context: ContextTypes.DEFAULT_TYPE):
     server.logger.info(f"Generating ALL GAMES for client: {update.effective_chat.first_name} - {update.effective_chat.username}: {update.effective_chat.id}")
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Currently generating for all games\.\.\.", parse_mode='MARKDOWNV2')
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Come back in about 5\-10 minutes\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Currently generating for all games\\.\\.\\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Come Back in about 5\\-10 minutes\\.", parse_mode='MARKDOWNV2')
 
-    tasks = [game_handler(update, context, i + 1, True, i * 30) for i in range(8)]
+    # Wait a certain number of seconds between each game
+    tasks = [game_handler(update, context, i + 1, True, i * 30) for i in range(len(server.GAMES))]
     await asyncio.gather(*tasks)
 
+
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(TOKEN or TOKEN_INSECURE).build()
+    application = ApplicationBuilder().token(TOKEN).build()
     server.logger.info("Server is running. Awaiting users...")
 
     application.add_handler(CommandHandler('start', start, block=False))
@@ -142,8 +128,11 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler('poly', poly, block=False))
     application.add_handler(CommandHandler('trim', trim, block=False))
     application.add_handler(CommandHandler('zoo', zoo, block=False))
-    application.add_handler(CommandHandler('fluf', all, block=False))
-    
+    application.add_handler(CommandHandler('tile', tile, block=False))
+    application.add_handler(CommandHandler('fluff', fluff, block=False))
+    application.add_handler(CommandHandler('stone', stone, block=False))
+
     application.add_handler(CommandHandler('all', all, block=False))
+
 
     application.run_polling()
